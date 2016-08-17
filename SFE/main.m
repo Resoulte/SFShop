74b2e81197a2b65bf6538e0d8d8ce14eb7db8c93
Resolute <sferic@163.com>
2016-08-17 11:32:26 +0800

    初始化项目

---
+++ SFE/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  SFE
+//
+//  Created by ma c on 16/8/17.
+//  Copyright © 2016年 shifei. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
